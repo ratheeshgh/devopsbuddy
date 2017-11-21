cf802846cf17898c9be94371fef711dfa2eff51e
Ratheesh <uratheesh@temenos.com>
2017-11-21 14:13:57 -0600

    Added logging set up with logback

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,16 +1,32 @@
 package com.devopsbuddy;
 
+import com.devopsbuddy.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootApplication
+@WebAppConfiguration
 public class DevopsbuddyApplicationTests {
+
+	@Autowired
+	private I18NService i18NService;
 
 	@Test
 	public void contextLoads() {
-	}
 
+		}
+
+		public void testMessageByLocalService() throws Exception{
+
+			String expectedResult = "Bootstrap Starter Template";
+			String messageId = "index.main.callout";
+			String actual = i18NService.getMessage(messageId);
+			Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
+		}
 }
